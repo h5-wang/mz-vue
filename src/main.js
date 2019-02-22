e9bbd4478941416dd97a0cc88ba364fed054353f
Your Name <you@example.com>
2019-02-22 17:26:47 +0800

    路由拦截，各个路由导航守卫的使用

--- src/main.js
+++ src/main.js
@@ -2,6 +2,10 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router.js'
 import './assets/styles/reset.less';
+
+// 引入的 nprogress.css 文件必须要放在 common.less 文件前面
+import 'nprogress/nprogress.css';
+import './assets/styles/common.less';
 // 让浏览器的console里不会出现开发提示
 Vue.config.productionTip = false;
 
--- src/router.js
+++ src/router.js
@@ -16,10 +16,17 @@ import Center from './views/Center.vue';
 import City from './views/City.vue';
 import Home from './views/Home.vue';
 import Detail from './views/Detail.vue';
+import Login from './views/login.vue';
+
+// 引入顶部滚动条
+import Nprogress from 'nprogress';
+
+// 设置顶部进度条右侧出现小圆圈 默认true
+Nprogress.configure({ showSpinner: false });
 
 Vue.use(VueRouter); // 这个方法会调用 vueRouter 的 install 方法，而 install 方法里面就有做全局配置，所以路由可以在全局使用，而且不用注册
 let router = new VueRouter({
-  mode: 'history',
+  mode: 'hash',
 
   //  路由对照表   url ->  视图组件
   /** localhost:8080/#/Fmles    ->  Fimles.vue
@@ -79,6 +86,73 @@ let router = new VueRouter({
         age: 19
       }
     },
+    {
+      path: '/card/:id',
+      component: {
+        render (h) {
+          var that = this;
+          return h('div', [
+            '卖座卡页面',
+            h('button', {
+              on: {
+                click: function () {
+                  console.log('我被点击了');
+                  that.reload();
+                }
+              }
+            }, [
+              '我的天',
+              h('span', '我是一个标签')
+            ])
+          ])
+        },
+        methods: {
+          reload () {
+            console.log('我被执行了');
+            router.push('/card/李四');
+          }
+        },
+        // 组件内的路由守卫函数
+        beforeRouteEnter (to, from, next) {
+          console.log('Enter');
+          next();
+        },
+        // 只会在页面使用了路由参数的时候才会被触发  /card/100  ->  /card/200
+        beforeRouteUpdate (to, from, next) {
+          console.log('Update');
+          next();
+        },
+        beforeRouteLeave (to, from, next) {
+          console.log('Leave');
+          next();
+        }
+      }
+      // 路由独自享有的钩子函数 使用时需要关掉全局前置守卫和后置守卫
+      // beforeEnter (to, from, next) {
+      //   // 设置false 会导致路由被拦截，无法进入 card 页面
+      //   next(false);
+      // }
+    },
+    {
+      path: '/money',
+      component: {
+        render (h) {
+          return h('div', '余额页面')
+        }
+      }
+    },
+    {
+      path: '/system',
+      component: {
+        render (h) {
+          return h('div', '设置页面')
+        }
+      }
+    },
+    {
+      path: '/login',
+      component: Login
+    },
     // 设置一个通配符的 一级路由，当url地址无法与上面的规则匹配的时候，就会和它匹配
     {
       path: '*',
@@ -103,4 +177,33 @@ let router = new VueRouter({
   ]
 })
 
+// 全局前置守卫
+/*
+  路由守卫中
+  a ->  b
+  to 将要去的路由的路由对象         a
+  from  从哪里来的路由的路由对象    b
+  next  是否允许去       next() 允许    next（false）  不允许（或则不使用next）
+*/
+/*
+router.beforeEach((to, from, next) => {
+  // 调用 Nprogress 的 start 方法
+  Nprogress.start();
+
+  if (to.path === '/card' || to.path === '/money' || to.path === '/system') {
+    // next(false);
+    // next('/login');  字符串模式
+    next({
+      // 对象模式
+      path: '/login'
+    })
+  } else {
+    next();
+  }
+})
+*/
+// 全局后置守卫
+// router.afterEach((to, from, next) => {
+//   Nprogress.done();
+// })
 export default router;
